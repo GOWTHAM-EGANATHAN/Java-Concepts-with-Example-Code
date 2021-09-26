5f67f957cb97ec1cdeb34f0bdcaa6c0c9207f96d
Gowtham Eganathan <51506726+GOWTHAM-EGANATHAN@users.noreply.github.com>
2021-09-26 10:40:39 +0530

    Update method overloading.java

--- method overloading.java
+++ method overloading.java
@@ -1,1 +1,30 @@
+import java.util.*;
 
+class Test
+{
+    public static int sum(int a,int b)
+    {
+        return a + b;
+    }
+    public static int sum(int a,int b,int c)
+    {
+        return a + b + c;
+    }
+    public static float sum(float a1, float a2, float a3, float a4)
+    {
+        return a1 + a2 + a3 + a4;
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z = sc.nextInt();
+        float a1 = sc.nextFloat();
+
+        System.out.println("sum of x and y = "+sum(x,y));
+        System.out.println("sum of x,y and z = "+sum(x,y,z));
+        System.out.println("sum of x,y,z and a1 = "+sum(x,y,z,a1));
+    }
+}
