71639c10e95d7338dd3c2936514d4687e884891c
Gowtham Eganathan <51506726+GOWTHAM-EGANATHAN@users.noreply.github.com>
2021-10-02 00:15:01 +0530

    Update instaceintBlock.java

--- instaceintBlock.java
+++ instaceintBlock.java
@@ -1,1 +1,23 @@
-
+import java.util.*;
+class MyFirstClass{
+    MyFirstClass(){
+        System.out.println("Inside parent class");
+    }
+    {
+        System.out.println("Inside Block parent class");
+    }
+}
+class MySecondClass extends MyFirstClass{
+    MySecondClass(){
+        System.out.println("Inside second class");
+    }
+    {
+        System.out.println("Block Inside second class");
+    }
+}
+class MainClass{
+    public static void main(String[] args){
+        System.out.println("Inide main class");
+        MySecondClass MySec = new MySecondClass();
+    }
+}
