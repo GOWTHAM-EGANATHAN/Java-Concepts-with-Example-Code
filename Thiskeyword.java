dd3525b2dbaebea5a6dbe121e71beac6374b3782
Gowtham Eganathan <51506726+GOWTHAM-EGANATHAN@users.noreply.github.com>
2021-10-06 23:38:21 +0530

    Update Thiskeyword.java

--- Thiskeyword.java
+++ Thiskeyword.java
@@ -1,1 +1,16 @@
+//This keyword is used to refer current object inside a method or constructor
 import java.util.*;
+class MyFirstClass{
+    int x,y;
+    MyFirstClass(int x,int y){
+        this.x = x;
+        this.y = y;
+    }
+
+}
+class MainClass{
+    public static void main(String[] args){
+        MyFirstClass Mfc = new MyFirstClass(10,20);
+        System.out.println("object value :"+Mfc.y);
+    }
+}
